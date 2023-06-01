60989a8dc71977508ea04c3f9e182ab19c07a736
Rachel Pak <rachelpak00@gmail.com>
2023-05-31 20:44:43 -0500

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   
   const [puppies, setPuppies] = useState(puppyList)
-  console.log('puppyList: ', puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
-      <div className = "App">
+      <div className = "container">
       { 
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })
       }
+       {featPupId && (
+        <div className='featured-container'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
   );
 }
